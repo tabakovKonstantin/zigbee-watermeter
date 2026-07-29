c65adc679ab58ad85d90b67ad856eca6d8f048f6
Konstantin Tabakov <konkov90@gmail.com>
2026-07-29 21:11:57 +0300

    Extract battery monitoring module

--- main/watermeter.c
+++ main/watermeter.c
@@ -4,9 +4,7 @@
 #include <string.h>
 
 #include "driver/gpio.h"
-#include "esp_adc/adc_oneshot.h"
 #include "esp_app_desc.h"
-#include "esp_check.h"
 #include "esp_log.h"
 #include "esp_ota_ops.h"
 #include "esp_pm.h"
@@ -28,12 +26,11 @@
 #include "zcl/esp_zigbee_zcl_poll_control.h"
 #include "zcl/esp_zigbee_zcl_power_config.h"
 
-#include "battery_math.h"
+#include "battery.h"
 #include "meter_math.h"
 #include "meter_state.h"
 
 #define SENSOR_PIN ((gpio_num_t)CONFIG_WATERMETER_SENSOR_GPIO)
-#define BATTERY_ADC_CHANNEL ADC_CHANNEL_0
 #define HA_ENDPOINT 1
 
 #define ESP_MANUFACTURER_NAME "ZigbeeHive"
@@ -52,9 +49,6 @@
 #define SENSOR_RELEASE_POLL_MS 20
 #define SENSOR_RELEASE_STABLE_MS 50
 #define SENSOR_RELEASE_TIMEOUT_MS (30 * 1000)
-#define BATTERY_ADC_DISCARD_SAMPLES 1
-#define BATTERY_ADC_AVG_SAMPLES 16
-#define BATTERY_MAX_REASONABLE_MV 4300U
 #define SENSOR_QUEUE_LEN 8
 #define ESP_ZB_PRIMARY_CHANNEL_MASK ESP_ZB_TRANSCEIVER_ALL_CHANNELS_MASK
 
@@ -90,12 +84,6 @@ typedef struct {
 } zigbee_runtime_t;
 
 typedef struct {
-    adc_oneshot_unit_handle_t adc_handle;
-    uint8_t voltage_zcl;
-    uint8_t percent_zcl;
-} battery_runtime_t;
-
-typedef struct {
     esp_zb_uint48_t current_summation;
     esp_zb_uint48_t scaled_summation;
     esp_zb_uint24_t multiplier;
@@ -116,7 +104,6 @@ typedef struct {
 static QueueHandle_t s_sensor_queue;
 static esp_sleep_wakeup_cause_t s_boot_wakeup_cause;
 static zigbee_runtime_t s_zigbee;
-static battery_runtime_t s_battery;
 static meter_zcl_attrs_t s_meter_attrs;
 static ota_state_t s_ota;
 
@@ -337,70 +324,6 @@ static esp_err_t ota_upgrade_handler(esp_zb_zcl_ota_upgrade_value_message_t *mes
     return ESP_OK;
 }
 
-static uint32_t battery_read_mv(void)
-{
-    if (!s_battery.adc_handle) {
-        ESP_LOGW(TAG, "Battery ADC read failed on A0/D0/GPIO0: ADC is not initialized");
-        return 0;
-    }
-
-    uint32_t raw_sum = 0;
-    int raw = 0;
-    for (uint32_t i = 0; i < (BATTERY_ADC_DISCARD_SAMPLES + BATTERY_ADC_AVG_SAMPLES); i++) {
-        esp_err_t err = adc_oneshot_read(s_battery.adc_handle, BATTERY_ADC_CHANNEL, &raw);
-        if (err != ESP_OK) {
-            ESP_LOGW(TAG, "Battery ADC read failed on A0/D0/GPIO0: %s", esp_err_to_name(err));
-            return 0;
-        }
-        if (i >= BATTERY_ADC_DISCARD_SAMPLES) {
-            raw_sum += (uint32_t)raw;
-        }
-    }
-
-    uint32_t raw_avg = raw_sum / BATTERY_ADC_AVG_SAMPLES;
-    uint32_t adc_mv = battery_gpio_mv_from_raw(raw_avg);
-    uint32_t battery_mv = battery_mv_from_gpio_mv(adc_mv);
-    if (battery_mv > BATTERY_MAX_REASONABLE_MV) {
-        ESP_LOGW(TAG,
-                 "Battery ADC A0/D0/GPIO0 saturated/clamped: raw_avg=%" PRIu32 " adc=%" PRIu32
-                 "mV battery=%" PRIu32 "mV clamped=%umV",
-                 raw_avg, adc_mv, battery_mv, BATTERY_MAX_REASONABLE_MV);
-        battery_mv = BATTERY_MAX_REASONABLE_MV;
-    } else {
-        ESP_LOGI(TAG, "Battery ADC A0/D0/GPIO0: raw_avg=%" PRIu32 " adc=%" PRIu32 "mV battery=%" PRIu32 "mV",
-                 raw_avg, adc_mv, battery_mv);
-    }
-    return battery_mv;
-}
-
-static void battery_update_attr(void)
-{
-    uint32_t battery_mv = battery_read_mv();
-    if (battery_mv == 0) {
-        return;
-    }
-
-    uint8_t percent = battery_percent_from_mv(battery_mv);
-    s_battery.voltage_zcl = (uint8_t)((battery_mv + 50U) / 100U);
-    s_battery.percent_zcl = (uint8_t)(percent * 2U);
-
-    esp_zb_zcl_set_attribute_val(HA_ENDPOINT,
-                                 ESP_ZB_ZCL_CLUSTER_ID_POWER_CONFIG,
-                                 ESP_ZB_ZCL_CLUSTER_SERVER_ROLE,
-                                 ESP_ZB_ZCL_ATTR_POWER_CONFIG_BATTERY_VOLTAGE_ID,
-                                 &s_battery.voltage_zcl,
-                                 false);
-
-    esp_zb_zcl_set_attribute_val(HA_ENDPOINT,
-                                 ESP_ZB_ZCL_CLUSTER_ID_POWER_CONFIG,
-                                 ESP_ZB_ZCL_CLUSTER_SERVER_ROLE,
-                                 ESP_ZB_ZCL_ATTR_POWER_CONFIG_BATTERY_PERCENTAGE_REMAINING_ID,
-                                 &s_battery.percent_zcl,
-                                 false);
-
-    ESP_LOGI(TAG, "Battery: %" PRIu32 "mV %u%%", battery_mv, percent);
-}
-
 static void report_attr(uint16_t cluster_id, uint16_t attr_id, bool manufacturer_specific)
 {
     esp_zb_zcl_report_attr_cmd_t cmd = {
@@ -489,7 +412,7 @@ static void meter_update_zigbee_attrs(bool send_report, bool include_battery)
                                  false);
 
     if (include_battery) {
-        battery_update_attr();
+        battery_update_zigbee_attrs(HA_ENDPOINT);
     }
 
     if (send_report) {
@@ -740,20 +663,6 @@ static void sensor_task(void *pvParameters)
         finish_sensor_pulse_before_sleep();
         last_pulse_us = esp_timer_get_time();
     }
-}
-
-static esp_err_t adc_init(void)
-{
-    adc_oneshot_unit_init_cfg_t init_config = {
-        .unit_id = ADC_UNIT_1,
-    };
-    ESP_RETURN_ON_ERROR(adc_oneshot_new_unit(&init_config, &s_battery.adc_handle), TAG, "adc unit");
-
-    adc_oneshot_chan_cfg_t chan_config = {
-        .bitwidth = ADC_BITWIDTH_DEFAULT,
-        .atten = ADC_ATTEN_DB_12,
-    };
-    return adc_oneshot_config_channel(s_battery.adc_handle, BATTERY_ADC_CHANNEL, &chan_config);
 }
 
 #if CONFIG_WATERMETER_SLEEP_MODE_LIGHT
@@ -1047,8 +956,6 @@ static void zigbee_add_basic_cluster(esp_zb_cluster_list_t *cluster_list)
 static void zigbee_add_power_config_cluster(esp_zb_cluster_list_t *cluster_list)
 {
     esp_zb_attribute_list_t *power_attr_list = esp_zb_zcl_attr_list_create(ESP_ZB_ZCL_CLUSTER_ID_POWER_CONFIG);
-    s_battery.voltage_zcl = 0;
-    s_battery.percent_zcl = 0;
     static uint8_t battery_size = ESP_ZB_ZCL_POWER_CONFIG_BATTERY_SIZE_BUILT_IN;
     static uint8_t battery_quantity = 1;
     static uint8_t battery_rated_voltage = 37;
@@ -1057,13 +964,13 @@ static void zigbee_add_power_config_cluster(esp_zb_cluster_list_t *cluster_list)
                                             ESP_ZB_ZCL_ATTR_POWER_CONFIG_BATTERY_VOLTAGE_ID,
                                             ESP_ZB_ZCL_ATTR_TYPE_U8,
                                             ESP_ZB_ZCL_ATTR_ACCESS_READ_ONLY | ESP_ZB_ZCL_ATTR_ACCESS_REPORTING,
-                                            &s_battery.voltage_zcl));
+                                            battery_voltage_zcl_attr()));
     ESP_ERROR_CHECK(esp_zb_cluster_add_attr(power_attr_list,
                                             ESP_ZB_ZCL_CLUSTER_ID_POWER_CONFIG,
                                             ESP_ZB_ZCL_ATTR_POWER_CONFIG_BATTERY_PERCENTAGE_REMAINING_ID,
                                             ESP_ZB_ZCL_ATTR_TYPE_U8,
                                             ESP_ZB_ZCL_ATTR_ACCESS_READ_ONLY | ESP_ZB_ZCL_ATTR_ACCESS_REPORTING,
-                                            &s_battery.percent_zcl));
+                                            battery_percent_zcl_attr()));
     ESP_ERROR_CHECK(esp_zb_cluster_add_attr(power_attr_list,
                                             ESP_ZB_ZCL_CLUSTER_ID_POWER_CONFIG,
                                             ESP_ZB_ZCL_ATTR_POWER_CONFIG_BATTERY_SIZE_ID,
@@ -1200,8 +1107,8 @@ void app_main(void)
     ESP_ERROR_CHECK(meter_state_load());
     ESP_LOGI(TAG, "app_main: meter state ok");
 
-    ESP_ERROR_CHECK(adc_init());
-    ESP_LOGI(TAG, "app_main: battery adc ok on ADC1 channel %d", BATTERY_ADC_CHANNEL);
+    ESP_ERROR_CHECK(battery_init());
+    ESP_LOGI(TAG, "app_main: battery adc ok on ADC1 channel 0");
 
 #if CONFIG_WATERMETER_SLEEP_MODE_LIGHT
     ESP_ERROR_CHECK(power_management_init());
